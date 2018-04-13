b47851c20e72061a1dd71db4c239034e67f88a43
Abhishek Karan <karan_abhi@live.in>
2018-04-13 01:07:58 -0700

    Google Maps working but css issue, state updation of source & destination issue

---
+++ busTracker/assets/js/gMap.js
@@ -0,0 +1,32 @@
+export default function showGMap(lat1,long1,lat2,long2){
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var directionsService = new google.maps.DirectionsService();
+
+  var map;
+
+  var mySrc = new google.maps.LatLng(lat1, long1);//Abington
+  var myDest = new google.maps.LatLng(lat2, long2);//AirPort
+
+  var mapOptions = {
+    zoom: 15,
+    center: mySrc
+  };
+
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  directionsDisplay.setMap(map);
+showMap(mySrc,myDest,directionsService,directionsDisplay);
+}
+
+function showMap(mySrc,myDest,directionsService,directionsDisplay) {
+  //alert("aa")
+  var request = {
+    origin: mySrc,
+    destination: myDest,
+    travelMode: 'TRANSIT'
+  };
+
+  directionsService.route(request, function (result, status) {
+    directionsDisplay.setDirections(result);
+  });
+
+}//showMap ends
---
+++ busTracker/assets/js/tracker.js
@@ -0,0 +1,132 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'reactstrap';
+import { Typeahead } from 'react-bootstrap-typeahead';
+import classnames from 'classnames';
+import showGMap from './gMap';
+
+export default function tracker_init(root, channel) {
+	ReactDOM.render(<Tracker channel={channel}/>, root);
+}
+
+class Tracker extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.channel = props.channel;
+		this.renderStops = this.renderStops.bind(this);
+		this.handleSourceChange = this.handleSourceChange.bind(this);
+		this.handleDestinationChange = this.handleDestinationChange.bind(this);
+
+		this.state ={
+			stops: [],
+			source:{},
+			destination:[]
+		}
+
+		this.channel.join()
+		.receive("ok",this.createState.bind(this))
+		.receive("error",resp => "Error while joining");
+	}
+
+	createState(st1){
+		var c = {
+			stops: st1.stops
+		}
+		this.setState(c);
+	}
+
+	renderStops(stops) {
+		if(stops != null){
+
+			return $.each(stops, function(stop,index){
+				return (
+					<option key={index}>{stop}</option>
+				);
+
+			});
+		}
+
+	}
+
+	render() {
+		var stops = "";
+		var stops2 = "";
+		if(this.state.stops)
+		{
+			console.log("Stops working!");
+		stops = this.state.stops.map(stop => (
+			stop.id
+		));
+		stops2 = this.state.stops.map(stop => (
+			{ id: stop.id,
+				label: stop.attributes.name,
+				latitude: stop.attributes.latitude,
+				longitude: stop.attributes.longitude	}
+			));
+		}
+
+		//showGMap(42.107156,42.107156,42.107156,42.107156);
+
+		return(
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<Fragment>
+							<Typeahead options={stops2} placeholder="Choose a source station..." valueKey="id"
+						   	onChange={selected => {this.handleSourceChange(selected);}}/>
+						</Fragment>
+			  	</div>
+				</div>
+				<br/>
+				<div className="row">
+					<div className="col">
+						<Fragment>
+							<Typeahead	options={stops2} placeholder="Choose a Destination station..." valueKey="id"
+							  	onChange={selected => {this.handleDestinationChange(selected);}}/>
+						</Fragment>
+					</div>
+				</div>
+				<br/>
+				<div className="row">
+					<div className="col" align="center">
+						<button id="showMapBtn"  className="btn btn-success btn-md center-block"
+						onClick={this.handleGMap.bind(this)}>Get Routes!</button>
+				  </div>
+				</div>
+				<br/><hr/>
+				<div id="map-canvas" className=""></div>
+			</div>);
+	}
+	handleSourceChange(x){
+		var c = {
+			source: {id: x[0].id, label: x[0].label, latitude: x[0].latitude, longitude: x[0].longitude}
+		}
+		this.setState(c);
+		console.log(this.state);
+		//console.log(c);
+
+	}
+
+	handleDestinationChange(x){
+		var c = {
+			destination:x[0]
+		}
+		this.setState(c);
+		console.log(this.state.destination);
+	}
+
+	handleGMap(e){
+		//console.log(e.target);
+		//$("#map_canvas").hide();//Hide Map Initially
+		//var map = document.getElementById('map_canvas')
+		//google.maps.event.addDomListener(map, 'click', showGMap(42.107156,42.107156,42.107156,42.107156));
+		showGMap(42.107156,42.107156,42.107156,42.107156);
+	}
+}
+
+function Stop(params){
+
+	return (<option value={params.stop.attributes.name} id={params.key}>{params.stop.attributes.name}</option>);
+}
